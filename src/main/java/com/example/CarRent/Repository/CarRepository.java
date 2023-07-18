9a2f9a4f4a437546b5b571a533586260a6464672
munteanusebastian <117016982+munteanusebastian@users.noreply.github.com>
2023-07-18 18:16:23 +0300

    Car and Customer imp

---
+++ src/main/java/com/example/CarRent/Repository/CarRepository.java
@@ -0,0 +1,8 @@
+package com.example.CarRent.Repository;
+
+import com.example.CarRent.Model.Car;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CarRepository extends JpaRepository <Car, Integer> { }
